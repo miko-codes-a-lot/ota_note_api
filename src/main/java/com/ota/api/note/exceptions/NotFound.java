555bc10f63b3ce6c48a67c6ea4c936bdc318ef26
Miko Chu <miko.codecrafters@gmail.com>
2024-04-28 05:08:32 +0800

    added NoteFound as our domain exception to abstract controller from directly handling framework specific errors

---
+++ src/main/java/com/ota/api/note/exceptions/NotFound.java
@@ -0,0 +1,11 @@
+package com.ota.api.note.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class NotFound extends RuntimeException {
+    public NotFound(String s) {
+        super(s);
+    }
+}
