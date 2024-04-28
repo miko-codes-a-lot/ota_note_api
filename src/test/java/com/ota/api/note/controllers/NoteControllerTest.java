91430a246ab9fd5dea62d8ffb657e466ab65fd5e
Miko Chu <miko.codecrafters@gmail.com>
2024-04-29 05:07:02 +0800

    added implementation testing for HTTP GET /api/users/{id}

--- src/test/java/com/ota/api/note/controllers/NoteControllerTest.java
+++ src/test/java/com/ota/api/note/controllers/NoteControllerTest.java
@@ -1,8 +1,10 @@
 package com.ota.api.note.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ota.api.note.models.dto.NoteDTO;
 import com.ota.api.note.models.dto.PaginateParamsDTO;
 import com.ota.api.note.models.dto.PaginatedDTO;
+import com.ota.api.note.models.form.NoteForm;
 import com.ota.api.note.services.LogService;
 import com.ota.api.note.services.NoteService;
 import org.junit.jupiter.api.Test;
@@ -22,8 +24,11 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 import static com.ota.api.note.utils.DateUtils.toISOString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -39,6 +44,8 @@ public class NoteControllerTest {
 
     @MockBean
     private LogService logService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void NoteController_GetNotes_ReturnNotes() throws Exception {
@@ -70,5 +77,26 @@ public class NoteControllerTest {
                 .andExpect(jsonPath("$.totalItems", is(100)))
                 .andExpect(jsonPath("$.totalPages", is(10)))
                 .andExpect(jsonPath("$.items").isArray());
+    }
+
+    @Test
+    public void NoteController_GetNoteById_ReturnNote() throws Exception {
+        NoteDTO note = NoteDTO.builder()
+                .id(1L)
+                .title("Sample Title")
+                .body("Sample Body")
+                .dateCreated(toISOString(new Date()))
+                .dateUpdated(toISOString(new Date()))
+                .build();
+
+        when(noteService.findOne(anyLong())).thenReturn(note);
+
+        mockMvc.perform(get("/api/notes/{id}", 1L))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.title", is("Sample Title")))
+                .andExpect(jsonPath("$.body", is("Sample Body")));
     }
 }
