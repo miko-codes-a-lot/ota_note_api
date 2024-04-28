4ddb17c03c35eaa655bff8c1b33c010dd50bf6d1
Miko Chu <miko.codecrafters@gmail.com>
2024-04-28 23:07:40 +0800

    modified NoteController to implement HTTP GET all & HTTP POST

--- src/main/java/com/ota/api/note/controllers/NoteController.java
+++ src/main/java/com/ota/api/note/controllers/NoteController.java
@@ -1,11 +1,13 @@
 package com.ota.api.note.controllers;
 
 import com.ota.api.note.Config;
-import com.ota.api.note.exceptions.NotFound;
+import com.ota.api.note.errors.NotFoundError;
 import com.ota.api.note.models.dto.ApiErrorDTO;
 import com.ota.api.note.models.dto.NoteDTO;
+import com.ota.api.note.models.dto.PaginateParamsDTO;
 import com.ota.api.note.models.dto.PaginatedDTO;
 import com.ota.api.note.models.form.NoteForm;
+import com.ota.api.note.services.NoteService;
 import com.ota.api.note.spring.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -19,11 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.ota.api.note.utils.JSONUtils.stringify;
 
 /**
  * Controller for managing notes.
@@ -43,10 +40,12 @@ public class NoteController {
     private static final String NOTE_NOT_FOUND = "Note not found.";
 
     private final Config config;
+    private final NoteService noteService;
 
     @Autowired
-    public NoteController(Config config) {
+    public NoteController(Config config, NoteService noteService) {
         this.config = config;
+        this.noteService = noteService;
     }
 
 
@@ -60,13 +59,17 @@ public class NoteController {
     @GetMapping("/{id}")
     public ResponseEntity<NoteDTO> findById(@PathVariable long id) {
         if (id == 0) {
-            throw new NotFound(NOTE_NOT_FOUND);
+            throw new NotFoundError(NOTE_NOT_FOUND);
         }
 
         log.info(STR."username: \{config.tracingIdKey()}");
 
         return new ResponseEntity<>(
-                new NoteDTO(10L, "", ""),
+                NoteDTO.builder()
+                        .id(10L)
+                        .title("")
+                        .body("")
+                        .build(),
                 HttpStatus.OK
         );
     }
@@ -78,33 +81,16 @@ public class NoteController {
             @ApiResponse(responseCode = "500", description = "Internal server error", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorDTO.class)) })
     })
     @GetMapping("/")
-    public ResponseEntity<PaginatedDTO<NoteDTO>> findAll(
-            @RequestParam(value = "pageIndex", defaultValue = "1") int pageIndex,
-            @RequestParam(value = "sizeSize", defaultValue = "10") int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id") String sortBy,
-            @RequestParam(value = "search", defaultValue = "") String search
-    ) {
-        List<NoteDTO> notes = new ArrayList<>();
-        notes.add(new NoteDTO(10L, "a1", "b1"));
-        notes.add(new NoteDTO(11L, "a2", "b2"));
-        notes.add(new NoteDTO(12L, "a3", "b3"));
-        notes.add(new NoteDTO(13L, "a4", "b4"));
-        notes.add(new NoteDTO(14L, "a5", "b5"));
-
-        val paginate = PaginatedDTO.<NoteDTO>builder()
-                .data(notes)
-                .currentPage(1)
-                .totalPages(5)
-                .totalItems(50)
-                .build();
+    public ResponseEntity<PaginatedDTO<NoteDTO>> findAll(PaginateParamsDTO paginateParams) {
+        val notePage = this.noteService.findAll(paginateParams);
 
         return Response.<PaginatedDTO<NoteDTO>>builder()
                 .status(HttpStatus.OK)
-                .body(paginate)
+                .body(notePage)
                 .build();
     }
 
-    @Operation(summary = "Create a new note")
+    @Operation(summary = "Create a new note, ignores the id")
     @ApiResponses({
             @ApiResponse(responseCode = "201", description = "Successfully created the note"),
             @ApiResponse(responseCode = "400", description = "Invalid request body", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorDTO.class)) }),
@@ -114,16 +100,12 @@ public class NoteController {
     @PostMapping("/")
     public ResponseEntity<NoteDTO> create(@Valid @RequestBody NoteForm noteForm) {
         val note = NoteDTO.builder()
-                .id(noteForm.getId())
                 .title(noteForm.getTitle())
                 .body(noteForm.getBody())
                 .build();
 
-        log.info("micooo");
-        log.info(stringify(note));
-
         return Response.<NoteDTO>builder()
-                .body(note)
+                .body(this.noteService.create(note))
                 .status(HttpStatus.CREATED)
                 .build();
     }
@@ -160,7 +142,7 @@ public class NoteController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) throws Exception {
         if (id == 0) {
-            throw new NotFound(NOTE_NOT_FOUND);
+            throw new NotFoundError(NOTE_NOT_FOUND);
         }
 
         return Response.builder()
