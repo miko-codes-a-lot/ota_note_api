57dca9f5809fbe1f41bfa9d604dc1a58878f23d0
Miko Chu <miko.codecrafters@gmail.com>
2024-04-29 02:43:51 +0800

    added doc for custom errors

--- src/main/java/com/ota/api/note/errors/NotFoundError.java
+++ src/main/java/com/ota/api/note/errors/NotFoundError.java
@@ -3,6 +3,14 @@ package com.ota.api.note.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom error class designed to abstract away the complexity of Spring Framework errors
+ * and provide user-friendly error messages for resource not found scenarios.
+ * This class extends RuntimeException and sets the HTTP status code to NOT_FOUND.
+ *
+ * @author Miko Chu
+ * @since 2024-04-27
+ */
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class NotFoundError extends RuntimeException {
     public NotFoundError(String s) {
--- src/main/java/com/ota/api/note/errors/SimpleError.java
+++ src/main/java/com/ota/api/note/errors/SimpleError.java
@@ -3,6 +3,14 @@ package com.ota.api.note.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom error class designed to abstract away the complexity of Spring Framework errors
+ * and provide user-friendly error messages to clients.
+ * This class extends RuntimeException and sets the HTTP status code to BAD_REQUEST.
+ *
+ * @author Miko Chu
+ * @since 2024-04-28
+ */
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class SimpleError extends RuntimeException {
     public SimpleError(String message) {
