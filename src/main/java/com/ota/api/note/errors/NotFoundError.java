7bb13ec20493e215b2a0b4cfea5b6a12a94f7f28
Miko Chu <miko.codecrafters@gmail.com>
2024-04-28 22:58:44 +0800

    renamed NotFound to NotFoundError for consistency & changed package name to errors

--- src/main/java/com/ota/api/note/errors/NotFoundError.java
+++ src/main/java/com/ota/api/note/errors/NotFoundError.java
@@ -1,11 +1,11 @@
-package com.ota.api.note.exceptions;
+package com.ota.api.note.errors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class NotFound extends RuntimeException {
-    public NotFound(String s) {
+public class NotFoundError extends RuntimeException {
+    public NotFoundError(String s) {
         super(s);
     }
 }
