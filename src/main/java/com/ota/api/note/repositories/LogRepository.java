4b21a3dbeaf1a54962c2509383eb419e543505d8
Miko Chu <miko.codecrafters@gmail.com>
2024-05-07 00:13:06 +0800

    modified LogRepository to add a simple documentation

--- src/main/java/com/ota/api/note/repositories/LogRepository.java
+++ src/main/java/com/ota/api/note/repositories/LogRepository.java
@@ -3,5 +3,16 @@ package com.ota.api.note.repositories;
 import com.ota.api.note.models.entity.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * <p>The {@code LogRepository} extends {@link JpaRepository},
+ * which provides methods for basic CRUD operations and querying capabilities.
+ *
+ * <p>Through this repository, you can perform operations such as saving new log entries,
+ * retrieving log entries by their unique identifiers, updating existing log entries,
+ * deleting log entries, and performing various other queries as needed.
+ *
+ * @author Miko Chu
+ * @since 2024-04-28
+ */
 public interface LogRepository extends JpaRepository<Log, Long> {
 }
